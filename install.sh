b5a12d7a5d37e79c439409967bd6a77cb8f35a8a
Radovan Bast <bast@users.noreply.github.com>
2021-02-07 13:40:40 +0100

    use mktemp to avoid a rm

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/gitconfig ~/.gitconfig
 
 # install omf following https://github.com/oh-my-fish/oh-my-fish#installation
-curl -L https://get.oh-my.fish > install
-fish install --path=~/.local/share/omf --config=$PWD/omf
-rm -f install
+install_file=$(mktemp)
+curl -L https://get.oh-my.fish > $install_file
+fish $install_file --path=~/.local/share/omf --config=$PWD/omf
 
 # replace default theme with my preferred one
 rm -rf ~/.local/share/omf/themes/default/
